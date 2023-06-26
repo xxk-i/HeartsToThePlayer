ecb675d9782a601e6cb66d371fe355277eca024c
xxk-i <jeremy.r.jacobson@gmail.com>
2023-06-26 14:14:35 -0400

    Nether localization

--- src/stats_en.js
+++ src/stats_en.js
@@ -4,6 +4,7 @@ export let stats_en = {
   "displayName": "Name",
   "skinUrl": "Skin",
   "timeInWater": "Time In Water",
+  "timeInNether": "Time In Nether",
   "damageTaken": "Damage Taken",
   "mobsKilled": "Mobs Killed",
   "foodEaten": "Food Eaten",
