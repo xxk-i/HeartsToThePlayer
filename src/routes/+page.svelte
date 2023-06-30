4c28895a024d46e991f6d1179fd7962bb3953534
xxk-i <jeremy.r.jacobson@gmail.com>
2023-06-30 16:18:32 -0400

    Main page ready for release

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
 <script>
     import './../global.css'
+    import './styles.css'
+
+    import logo from '$assets/Logo.png';
 </script>
 
-<h1>COMING THIS SUMMER</h1>
+<body>
+    <div class='center'>
+        <img alt='Logo' src={logo}/>
+        <h1>COMING THIS SUMMER</h1>
+    </div>
+</body>
+
+
+<footer>
+    <h6>
+        hardercoremc.net 2023
+        <br>written in svelte &#139;3
+    </h6>
+
+</footer>
